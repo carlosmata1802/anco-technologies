347034972f8bbb129c7ebe62374ea37b8880270f
charli <jesus@alimx.mx>
2020-10-02 01:05:50 -0500

    Fix form function

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,6 +1,35 @@
 window.onload = () => {
     let loader = document.getElementsByClassName('loader')[0];
     setTimeout(() => {
-        loader.classList.add('invisible'); 
-    }, 1000);
+        loader.classList.add('invisible');
+    }, 800);
+}
+
+async function onSubmit(e) {
+    e.preventDefault();
+    const { name, company, phone, email, message } = e.target;
+
+    const data = {
+        name: name.value,
+        company: company.value,
+        phone: phone.value,
+        email: email.value,
+        message: message.value
+    }
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+    //let url = 'https://2svplln58k.execute-api.us-east-1.amazonaws.com/dev/sendEmail';
+    let url = 'http://localhost:3000/dev/sendEmail'
+    console.log(data)
+
+    const response = await fetch(url, options).then(x => x.json());
+    console.log(response)
+
 }
